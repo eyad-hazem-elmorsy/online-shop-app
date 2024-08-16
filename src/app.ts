a26414d5ec39146457bd0b27ee26137b911b4e1a
Eyad Hazem <eyadelmorsy16@gmail.com>
2024-08-17 00:43:08 +0300

    Determine assets path
    This commit determines assets path and adds some comments in server.ts file.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Express } from 'express';
+import path from 'path';
 
+// Create application
 const app: Express = express();
+
+// Determine assets path
+app.use(express.static(path.join(__dirname, 'assets')));
 
 // Setting a template engine
 app.set('view engine', 'ejs');
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './config.env' });
 
+// Import the application
 import app from './app';
 
+// Initialize port
 const port: number = Number(process.env.PORT) || 5000;
 
+// Server listener
 app.listen(port, (): void => {
     console.log(`App listening on port ${port}`);
 });
