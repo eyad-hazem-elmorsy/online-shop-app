e1b44dec9203df14da107bca8d27a9fefc6debfe
Eyad Hazem <eyadelmorsy16@gmail.com>
2024-12-30 23:31:36 +0200

    Provide database promise wrapper
    This commit povides a database promise wapper.

---
+++ src/Utils/DatabasePromiseWrapper.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+const dbUrl: string = process.env.DB_URL || 'mongodb://localhost:27017/online-shop';
+
+export default async<T> (promise: () => Promise<T>) => {
+    try {
+        await mongoose.connect(dbUrl);
+        const ret = await promise();
+        mongoose.disconnect();
+        return ret;
+    } catch(err) {
+        mongoose.disconnect();
+        throw err;
+    }
+}
