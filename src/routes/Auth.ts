d168f167ef4909f67155247b6f48311ae0c96339
Eyad Hazem <eyadelmorsy16@gmail.com>
2024-12-31 02:22:09 +0200

    Fix sign up post request url
    This commit fixes the post request of sign up url in auth route.

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -5,7 +5,7 @@ const router: Router = express.Router();
 
 // Signup middlewares
 router.get('/signup', authController.getSignup);
-router.post('/:id', express.urlencoded({extended: true}), authController.postSignup);
+router.post('/signup', express.urlencoded({extended: true}), authController.postSignup);
 
 // Login middlewares
 router.get('/login', authController.getLogin);
