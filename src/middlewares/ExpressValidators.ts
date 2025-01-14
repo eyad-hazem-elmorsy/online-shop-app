fd77e4d7a4ad4edaf02f26176067b949ceb865cc
Eyad Hazem <eyadelmorsy16@gmail.com>
2025-01-14 18:22:23 +0200

    Separate express validator middlewares
    This commit separates express validator middlewares to enhance reusability.

---
+++ src/middlewares/ExpressValidators.ts
@@ -0,0 +1,27 @@
+import { check } from 'express-validator';
+import { validationResult } from '.';
+
+export default {
+    signup: [
+        check('username')
+            .notEmpty()
+            .withMessage('Username is required')
+            .isLength({ min: 3 })
+            .withMessage('Length of username must be at least 3'),
+        check('email')
+            .notEmpty()
+            .withMessage('Email is required')
+            .isEmail()
+            .withMessage('Invalid email format'),
+        check('password')
+            .notEmpty()
+            .withMessage('Password is required')
+            .isLength({ min: 7 })
+            .withMessage('Length of username must be at least 7'),
+        check('confirmPassword').custom((value, { req }) => {
+            if (value === req.body.password) return true;
+            else throw new Error('Passwords are not equal');
+        }),
+        validationResult('/signup')
+    ]
+};
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,3 @@
 export { default as sessionMiddleware } from './Session';
 export { default as validationResult } from './ValidationResult';
+export { default as expressValidators } from './ExpressValidators';
